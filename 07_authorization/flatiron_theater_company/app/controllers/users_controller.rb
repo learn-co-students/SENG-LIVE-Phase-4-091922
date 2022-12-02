a1ed83b3f5a04b87a538535abffc4170b48e0472
Ix procopios <enprocopios@gmail.com>
2022-12-02 09:40:30 -0800

    Update users_controller.rb

--- 07_authorization/flatiron_theater_company/app/controllers/users_controller.rb
+++ 07_authorization/flatiron_theater_company/app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
     # skip_before_action 
 
     def show 
-        user = User.find_by(name:params[:name])
+        user = User.find(params[:id])
         render json: user, status: :ok
     end 
 
